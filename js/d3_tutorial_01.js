4f0c18b6f4f510db989682a93239290f8e83a8b4
RaimoHaikari <raimo.haikari@gmail.com>
2018-07-09 22:20:00 +0300

    Sivuston kopionti
    Kopioidaan kokeeksi omalla koneella ollut sivusto, jotta nähdään voitaisiinko sitä käyttää sellaisenaan.

---
+++ js/d3_tutorial_01.js
@@ -0,0 +1,8 @@
+var dataset = [1,2,3,4,5];
+
+d3.select('body')
+	.selectAll('p')
+	.data(dataset)
+	.enter()
+	.append('p') // appends paragraph for each data elementFromPoint
+	.text('D3 on mahtavaaa!');
